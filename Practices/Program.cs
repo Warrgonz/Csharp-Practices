78f4b92b713d3fa8f158019ad61ed65e2807ec9a
Warren Gonzalez <Warren0419@outlook.com>
2026-01-05 14:57:49 -0600

    First commit

---
+++ Practices/Program.cs
@@ -0,0 +1,36 @@
+﻿// dependencias
+
+// Este es el encargado de arrancar, orquestar y terminar el programa, solo hace los llamados (instancias) hacia los metodos.
+namespace Practices
+{
+    // Una clase no representa datos, representa comportamiento y estados
+    // Para pensar en un clase hay que deducir, "Que va hacer esta clase?" en vez de "Que campos tiene esta clase?"
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool menu = true;
+
+            while (menu)
+            {
+                Console.WriteLine("yuca:");
+                Console.WriteLine("Yuca1");
+                
+                string opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                    case "1":
+                        Console.WriteLine("Opt 1 Selected");
+                        break;
+                    case "2":
+                        Console.WriteLine("Successfully closed!");
+                        return;
+                }
+            }
+
+
+        }
+    }
+};
+
---
+++ Practices/TiposClases/Money.cs
@@ -0,0 +1,13 @@
+namespace Practices.TiposClases;
+
+public class Money
+{
+    public decimal Amount { get; set; }
+    public string? Currency { get; set; }
+    
+    public Money(decimal amount, string currency){
+        Amount = amount;
+        Currency = currency;
+    }
+    
+}
